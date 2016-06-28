0a36b420a7ac3ebbbb0048ec3a35288a29436dc7
Alexander Jacks and August von Trapp <alexander@>
2016-06-28 14:07:54 -0700

    adds functionality to handle all number inputs below 1000

--- lib/number_to_word.rb
+++ lib/number_to_word.rb
@@ -3,28 +3,39 @@ class String
 
     grouped_array = self.scan(/.../)
 
-    irregulars={11=>"eleven",12=>"twelve",13=>"thirteen",14=>"fourteen",15=>"fifteen",16=>"sixteen",17=>"seventeen",18=>"eighteen",19=>"nineteen"}
+    irregulars={10=>"ten", 11=>"eleven",12=>"twelve",13=>"thirteen",14=>"fourteen",15=>"fifteen",16=>"sixteen",17=>"seventeen",18=>"eighteen",19=>"nineteen"}
 
     tens={2=>"twenty",3=>"thirteen",4=>"forty",5=>"fifty",6=>"sixty",7=>"seventy",8=>"eighty",9=>"ninety"}
 
     ones={1=>"one",2=>"two",3=>"three",4=>"four",5=>"five",6=>"six",7=>"seven", 8=>"eight",9=>"nine"}
 
-    places = {   1 => "thousand", 2 => "million", 3 => "trillion"}
-    output_string = ''
+    places = {1 => "thousand", 2 => "million", 3 => "trillion"}
+
+    output_array = []
 
     grouped_array.each do |group|
       first_digit = group[0].to_i()
       second_digit = group[1].to_i()
       third_digit = group[2].to_i()
 
-
       if (first_digit > 0)
-        output_string += ones[first_digit]+' hundred'
+      output_array.push(ones[first_digit]+' hundred')
       end
 
+      if (second_digit == 1)
+        output_array.push(irregulars[group[1,2].to_i()])
+
+      elsif (second_digit > 1)
+        output_array.push(tens[second_digit])
+
+        if (third_digit > 0)
+          output_array.push(ones[third_digit])
+
+        end
+      end
     end
-    output_string
+  output_array.join(' ')
   end
 end
 
-puts "200".number_to_word
+puts "343".number_to_word
--- spec/number_to_word_spec.rb
+++ spec/number_to_word_spec.rb
@@ -3,11 +3,25 @@ require('number_to_word.rb')
 
 describe('String#number_to_word') do
 
-  it("given a number input longer than three digits, it returns numbers in groups of three digits") do
-    expect("234564789".number_to_word()).to(eq(["234","564","789"]))
-  end
+  # it("given a number input longer than three digits, it returns numbers in groups of three digits") do
+  #   expect("234564789".number_to_word()).to(eq(["234","564","789"]))
+  # end
 
   it('given input 200 it should return two hundred') do
     expect("200".number_to_word()).to(eq('two hundred'))
   end
+
+  it('given input 215 it should return "two hundred fifteen"') do
+    expect("215".number_to_word()).to(eq('two hundred fifteen'))
+  end
+
+  it('given input 340 it should return "three hundred forty"') do
+    expect("340".number_to_word()).to(eq('three hundred forty'))
+  end
+
+  it('given input 343 it should return "three hundred forty three"') do
+    expect("343".number_to_word()).to(eq('three hundred forty three'))
+  end
+
+
 end
