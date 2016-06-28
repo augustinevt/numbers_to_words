5591445ef5fc4ff6251d0d0dec25f945d6b3e556
Alexander Jacks and August von Trapp <alexander@>
2016-06-28 11:38:28 -0700

    second tdd, it splits into groups and return proper hundreds words"

--- lib/number_to_word.rb
+++ lib/number_to_word.rb
@@ -1,14 +1,30 @@
 class String
   def number_to_word
 
-    self.scan(/.../)
-    # irregulars={11=>"eleven",12=>"twelve",13=>"thirteen",14=>"fourteen",15=>"fifteen",16=>"sixteen",17=>"seventeen",18=>"eighteen",19=>"nineteen"}
-    #
-    # tens={2=>"twenty",3=>"thirteen",4=>"forty",5=>"fifty",6=>"sixty",7=>"seventy",8=>"eighty",9=>"ninety"}
-    #
-    # ones={1=>"one",2=>"two",3=>"three",4=>"four",5=>"five",6=>"six",7=>"seven", 8=>"eight",9=>"nine"}
+    grouped_array = self.scan(/.../)
 
+    irregulars={11=>"eleven",12=>"twelve",13=>"thirteen",14=>"fourteen",15=>"fifteen",16=>"sixteen",17=>"seventeen",18=>"eighteen",19=>"nineteen"}
+
+    tens={2=>"twenty",3=>"thirteen",4=>"forty",5=>"fifty",6=>"sixty",7=>"seventy",8=>"eighty",9=>"ninety"}
+
+    ones={1=>"one",2=>"two",3=>"three",4=>"four",5=>"five",6=>"six",7=>"seven", 8=>"eight",9=>"nine"}
+
+    places = {   1 => "thousand", 2 => "million", 3 => "trillion"}
+    output_string = ''
+
+    grouped_array.each do |group|
+      first_digit = group[0].to_i()
+      second_digit = group[1].to_i()
+      third_digit = group[2].to_i()
+
+
+      if (first_digit > 0)
+        output_string += ones[first_digit]+' hundred'
+      end
+
+    end
+    output_string
   end
 end
 
-puts "234564789".number_to_word
+puts "200".number_to_word
--- spec/number_to_word_spec.rb
+++ spec/number_to_word_spec.rb
@@ -2,8 +2,12 @@ require('rspec')
 require('number_to_word.rb')
 
 describe('String#number_to_word') do
+
   it("given a number input longer than three digits, it returns numbers in groups of three digits") do
     expect("234564789".number_to_word()).to(eq(["234","564","789"]))
   end
 
+  it('given input 200 it should return two hundred') do
+    expect("200".number_to_word()).to(eq('two hundred'))
+  end
 end
